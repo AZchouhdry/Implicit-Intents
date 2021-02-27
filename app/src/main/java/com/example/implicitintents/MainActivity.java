28a1188adea5dd987deefe9db95c0af5280a2fc7
AZchouhdry <abakar2017@namal.edu.pk>
2021-02-27 20:03:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.implicitintents;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        webview = findViewById(R.id.mywebview);
+        progressBar = findViewById(R.id.progressBar);
+        webview.loadUrl("https://www.namal.edu.pk/");
+
+        webview.setWebViewClient(new WebViewClient(){
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        webview.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBar.setProgress(newProgress);
+            }
+        });
+
+        Intent myintent = getIntent();
+        if (myintent!=null){
+            String URL = myintent.getData().toString();
+            webview.loadUrl(URL);
+        }
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (webview.canGoBack())
+        {
+            webview.goBack();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+
+    }
+}
